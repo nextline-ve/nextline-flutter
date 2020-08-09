3fa4eee3d6191c31c4deaaa4addec3dd7e66f73a
angel <angel@alphacode.com.br-at-503688510464>
2020-08-09 00:29:37 -0300

    widget de input general

---
+++ lib/widgets/jtext_field.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+
+typedef StringValue = String Function(String);
+
+class  JTextField extends  StatefulWidget {
+  final String label;
+  final bool isPass;
+  final Icon icon;
+  final TextInputType inputType;
+  final StringValue onValidator;
+  final StringValue onKeyValue;
+
+  const JTextField({
+    Key key,
+    @required this.label,
+    @required this.inputType,
+    @required this.isPass,
+    @required this.onValidator,
+    @required this.onKeyValue,
+    this.icon
+  }) : super(key: key);
+
+
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _JTextField();
+  }
+
+}
+
+class _JTextField extends State<JTextField> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(top: 20),
+      child: TextFormField(
+        autofocus: true,
+        keyboardType: widget.inputType,
+        validator: widget.onValidator,
+        onSaved: widget.onKeyValue,
+        decoration: InputDecoration(
+          prefixIcon: (widget.icon == null) ? null : Icon(
+              Icons.email,
+              color: Color.fromRGBO(2, 144, 223, 1)
+          ),
+          hintText: widget.label,
+          hintStyle: TextStyle(
+              color: Colors.grey,
+              fontFamily: 'fontInput'
+          ),
+          filled: true,
+          fillColor: Colors.white,
+          enabledBorder: UnderlineInputBorder(
+            borderRadius: BorderRadius.all(Radius.circular(25.0)),
+            borderSide: BorderSide(color: Colors.white),
+          ),
+          focusedBorder: UnderlineInputBorder(
+            borderRadius: BorderRadius.all(Radius.circular(25.0)),
+            borderSide: BorderSide(color: Colors.white),
+          ),
+        ),
+      ),
+    );
+  }
+
+}
