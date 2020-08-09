6f767881bfeccfc7ce5e973d72045af2f8a43c4d
angel <angel@alphacode.com.br-at-503688510464>
2020-08-09 04:13:55 -0300

    clic en solicitud de  servicio va  a  la pantalla. configuracion de  form de login.

--- lib/apps/Auth/ui/screens/login_screen.dart
+++ lib/apps/Auth/ui/screens/login_screen.dart
@@ -44,7 +44,6 @@ class _LoginScreen extends State<LoginScreen> {
   }
 
   void _setLogin() {
-    Scaffold.of(context)
-        .showSnackBar(SnackBar(content: Text('Processing Data')));
+    Navigator.pushNamed(context, '/select-service');
   }
 }
--- lib/apps/Auth/ui/widgets/form_login.dart
+++ lib/apps/Auth/ui/widgets/form_login.dart
@@ -11,6 +11,7 @@ class FormLogin extends StatefulWidget {
 }
 
 class _FormLogin extends State<FormLogin > {
+  final _formKey = GlobalKey<FormState>();
   String _email;
   String _pass;
   final RegExp emailRegex = new RegExp(
@@ -19,42 +20,45 @@ class _FormLogin extends State<FormLogin > {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return Container(
-      margin: EdgeInsets.all(8.0),
-      child: Column(
-        children: <Widget>[
-          JTextField(isPass: false,
-            label: "Correo Electrónico", inputType: TextInputType.emailAddress,
-            icon: Icon(Icons.email, color: Color.fromRGBO(2, 144, 223, 1)),
-            onKeyValue: (val) {
-              _email = val;
-              return val;
-            },
-            onValidator: (value) {
-              if (value.isEmpty) {
-                return 'Por favor escriba su correo electrónico';
-              }
-              if (!emailRegex.hasMatch(value)) {
-                return 'Ingrese un correo electrónico válido';
-              }
-              return null;
-            },
-          ),
-          JTextField(isPass: true,
-            label: "Contraseña", inputType: TextInputType.text,
-            icon: Icon(Icons.vpn_key, color: Color.fromRGBO(2, 144, 223, 1)),
-            onValidator: (value) {
-              if (value.isEmpty) {
-                return 'Por favor escriba su contraseña';
-              }
-              return null;
-            },
-            onKeyValue: (val) {
-              _pass = val;
-              return val;
-            },
-          ),
-        ],
+    return Form(
+      key: _formKey,
+      child: Container(
+        margin: EdgeInsets.all(8.0),
+        child: Column(
+          children: <Widget>[
+            JTextField(isPass: false,
+              label: "Correo Electrónico", inputType: TextInputType.emailAddress,
+              icon: Icon(Icons.email, color: Color.fromRGBO(2, 144, 223, 1)),
+              onKeyValue: (val) {
+                _email = val;
+                return val;
+              },
+              onValidator: (value) {
+                if (value.isEmpty) {
+                  return 'Por favor escriba su correo electrónico';
+                }
+                if (!emailRegex.hasMatch(value)) {
+                  return 'Ingrese un correo electrónico válido';
+                }
+                return null;
+              },
+            ),
+            JTextField(isPass: true,
+              label: "Contraseña", inputType: TextInputType.text,
+              icon: Icon(Icons.vpn_key, color: Color.fromRGBO(2, 144, 223, 1)),
+              onValidator: (value) {
+                if (value.isEmpty) {
+                  return 'Por favor escriba su contraseña';
+                }
+                return null;
+              },
+              onKeyValue: (val) {
+                _pass = val;
+                return val;
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
