6e608cfff4e803ef3e90787be4b6100a873a0750
angel <angel@alphacode.com.br-at-503688510464>
2020-08-09 10:54:09 -0300

    jbutton widget con ripple effect

--- lib/widgets/jbutton.dart
+++ lib/widgets/jbutton.dart
@@ -25,25 +25,33 @@ class _JButton extends State<JButton> {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return InkWell(
-      onTap: widget.onTab,
-      child: Container(
-        padding: EdgeInsets.all(10),
-        height: 60.0,
-        margin: EdgeInsets.only(top: widget.top, left: 10.0, right: 10.0),
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(25.0),
-          color: (widget.background == null)
-              ? Color.fromRGBO(82, 192, 242, 1)
-              : widget.background,
-        ),
-        child: Center(
-          child: Text(
-            widget.label,
-            textAlign: TextAlign.center,
-            style: TextStyle(
-                fontSize: 16.0, fontFamily: "fontTitle", color: Colors.white),
-          ),
+    return Container(
+      height: 60.0,
+      margin: EdgeInsets.only(top: widget.top, left: 10.0, right: 10.0),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(25.0),
+        color: (widget.background == null)
+            ? Color.fromRGBO(82, 192, 242, 1)
+            : widget.background,
+      ),
+      child: Material(
+        color: Colors.transparent,
+        child: InkWell(
+            onTap: widget.onTab,
+            child: Container(
+              padding: EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(25.0),
+              ),
+              child: Center(
+                child: Text(
+                  widget.label,
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      fontSize: 16.0, fontFamily: "fontTitle", color: Colors.white),
+                ),
+              ),
+            )
         ),
       ),
     );
