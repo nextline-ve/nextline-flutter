db5290abdf4c50f91456b359ec5013c6f4f3d7d6
Oscar <oacs1221@gmail.com>
2020-08-10 19:48:22 -0400

    Colors for the app on constants

---
+++ lib/utils/app_colors.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class AppColors {
+  static const Color ligth_blue_color = Color(0xFF52C0F2);
+  static const Color blue = Color(0xFF0090DF);
+  static const Color status_bar_color = Color(0xFF0070bd);
+  static const Color background_botom = Color(0xFF0071bf);
+  static const Color blue_dark = Color(0xFF005FAB);
+  static const Color green_color = Color(0xFF52E0A0);
+  static const Color mustard_color = Color(0xFFFCD581);
+  static const Color red_color = Color(0xFFD52941);
+  static const Color white_color = Color(0xFFF5F5F5);
+  static const Color gray_color = Color(0xFFedf6fb);
+  static const Color gray_text_color = Color(0xFF464545);
+}
