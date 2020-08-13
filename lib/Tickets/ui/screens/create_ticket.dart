249b8f42f1f4c3fd3fdb316c76e6fd189c7d9873
Oscar <oacs1221@gmail.com>
2020-08-12 23:51:41 -0400

    Finish ceate ticket

--- lib/Tickets/ui/screens/create_ticket.dart
+++ lib/Tickets/ui/screens/create_ticket.dart
@@ -1,6 +1,6 @@
 import 'package:basic_utils/basic_utils.dart';
 import 'package:flutter/material.dart';
-import 'package:nextline/Tickets/ui/widgets/add_ticket.dart';
+import 'package:nextline/Tickets/ui/widgets/button_icon.dart';
 import 'package:nextline/Tickets/ui/widgets/dropdown.dart';
 import 'package:nextline/utils/app_colors.dart';
 import 'package:nextline/utils/app_fonts.dart';
@@ -24,18 +24,39 @@ class _CreateTicketScreen extends State<CreateTicketScreen> {
         alignment: Alignment.center,
         children: <Widget>[
           Container(
+            padding: EdgeInsets.only(
+              top: 24,
+            ),
             child: Column(
               mainAxisAlignment: MainAxisAlignment.start,
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
                 _titleView("Nuevo Ticket"),
                 _date("01/08/2020"),
-                _inputLabel("Tipo de Avería"),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 24),
-                  child: DropdownWidget(),
+                _inputContainer(_inputLabel("Tipo de Avería"),
+                    DropdownWidget(hintText: "Seleccione una avería")),
+                _inputContainer(
+                  _inputLabel("Comentario2"),
+                  _textArea(
+                      "Explique en breves palabras el problema de su avería, y un técnico se pondrá en contacto con usted en un plazo de 24 horas."),
                 ),
-                _inputLabel("Comentario"),
+                Expanded(
+                  flex: 1,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 24, vertical: 24),
+                        child: ButtonIcon(
+                            icon: Icons.send,
+                            text: "VER TICKET",
+                            background: AppColors.green_color,
+                            height: 60),
+                      ),
+                    ],
+                  ),
+                )
               ],
             ),
           )
@@ -56,12 +77,15 @@ Widget _titleView(String text) {
 }
 
 Widget _date(String text) {
-  return Text(
-    text,
-    style: TextStyle(
-        fontFamily: AppFonts.poppins_bold,
-        fontSize: 12,
-        color: AppColors.gray_text_color),
+  return Padding(
+    padding: const EdgeInsets.symmetric(vertical: 12),
+    child: Text(
+      text,
+      style: TextStyle(
+          fontFamily: AppFonts.poppins_regular,
+          fontSize: 12,
+          color: AppColors.black_color),
+    ),
   );
 }
 
@@ -69,8 +93,59 @@ Widget _inputLabel(String text) {
   return Text(
     text.toUpperCase(),
     style: TextStyle(
-        fontFamily: AppFonts.poppins_bold,
+        fontFamily: AppFonts.subTitle,
         fontSize: 11,
         color: AppColors.blue_dark),
   );
+}
+
+Widget _textArea(String hinText) {
+  return Column(
+    children: <Widget>[
+      Container(
+        child: Container(
+            color: Colors.white,
+            child: Padding(
+              padding: EdgeInsets.all(8.0),
+              child: TextField(
+                maxLines: 8,
+                decoration: InputDecoration.collapsed(
+                    hintText: hinText,
+                    hintStyle: TextStyle(
+                        color: AppColors.black_color.withOpacity(0.34),
+                        fontSize: 12,
+                        fontFamily: AppFonts.poppins_regular)),
+              ),
+            )),
+      )
+    ],
+  );
+}
+
+Widget _inputContainer(Widget label, Widget input) {
+  return Container(
+      padding: EdgeInsets.only(left: 24, right: 24, bottom: 24),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: label,
+          ),
+          Container(
+              padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  boxShadow: [
+                    BoxShadow(
+                      blurRadius: 10,
+                      spreadRadius: 1,
+                      color: AppColors.blue_dark.withOpacity(0.15),
+                      offset: Offset(0, 5),
+                    )
+                  ],
+                  borderRadius: BorderRadius.circular(10)),
+              child: input)
+        ],
+      ));
 }
---
+++ lib/Tickets/ui/widgets/button_icon.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:nextline/utils/app_colors.dart';
+import 'package:nextline/utils/app_fonts.dart';
+
+class ButtonIcon extends StatefulWidget {
+  final IconData icon;
+  final Function textWidget;
+  final String text;
+  final Color background;
+  final Color iconColor;
+  final double height;
+  ButtonIcon({
+    Key key,
+    @required this.icon,
+    @required this.text,
+    this.height = 30,
+    this.textWidget = _buttonLabel,
+    this.background = AppColors.blue_dark,
+    this.iconColor = Colors.white,
+  }) : super(key: key);
+
+  @override
+  _ButtonIconState createState() => _ButtonIconState();
+}
+
+class _ButtonIconState extends State<ButtonIcon> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: this.widget.height,
+      padding: EdgeInsets.symmetric(horizontal: 23),
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(this.widget.height),
+          color: this.widget.background,
+          boxShadow: [
+            BoxShadow(
+                blurRadius: 10,
+                color: Colors.black.withOpacity(0.26),
+                offset: Offset(0, 10),
+                spreadRadius: 3)
+          ]),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Icon(this.widget.icon, color: this.widget.background),
+          this.widget.textWidget(this.widget.text, this.widget.iconColor),
+          Icon(
+            this.widget.icon,
+            color: this.widget.iconColor,
+          )
+        ],
+      ),
+    );
+  }
+}
+
+Widget _buttonLabel(String text, Color color) {
+  return Text(
+    text,
+    style: TextStyle(
+        fontFamily: AppFonts.poppins_regular, fontSize: 18, color: color),
+  );
+}
