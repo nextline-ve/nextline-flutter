7e47c90cab815d0799378a484f2d4709e27b39e2
Moises Serrano <moyca777@gmail.com>
2020-08-14 01:29:59 -0300

    Create declare_payment_screen.dart

---
+++ lib/DeclarePayment/ui/screens/declare_payment_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:nextline/utils/app_colors.dart';
+
+class DeclarePayment extends StatefulWidget {
+  @override
+  _DeclarePaymentState createState() => _DeclarePaymentState();
+}
+
+class _DeclarePaymentState extends State<DeclarePayment> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: AppColors.blue,
+      ),
+      body: ,
+    );
+  }
+}
