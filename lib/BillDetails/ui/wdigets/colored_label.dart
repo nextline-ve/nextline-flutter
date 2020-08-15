5686746ada6ca75f03aa0cc5fa34afa4681bb9e7
Moises Serrano <moyca777@gmail.com>
2020-08-15 00:00:43 -0300

    Update colored_label.dart

--- lib/BillDetails/ui/wdigets/colored_label.dart
+++ lib/BillDetails/ui/wdigets/colored_label.dart
@@ -4,7 +4,9 @@ import 'package:nextline/utils/app_fonts.dart';
 
 class ColoredLabel extends StatelessWidget {
   final String text;
-  const ColoredLabel({Key key, @required this.text}) : super(key: key);
+  final Color textColor;
+  const ColoredLabel({Key key, @required this.text, this.textColor})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -12,14 +14,10 @@ class ColoredLabel extends StatelessWidget {
       padding: EdgeInsets.symmetric(horizontal: 5, vertical: 2),
       decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(10),
-          color: AppColors.green_color
-      ),
+          color: AppColors.green_color),
       child: Text(
-        "             ${this.text}            " ,
-        style: TextStyle(
-          color: AppColors.white_color,
-          fontFamily:  AppFonts.fontTitle
-        ),
+        "             ${this.text}            ",
+        style: TextStyle(color: (this.textColor == null) ? AppColors.white_color : this.textColor, fontFamily: AppFonts.fontTitle),
       ),
     );
   }
