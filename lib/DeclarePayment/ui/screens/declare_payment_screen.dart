5a5985b2f54adbe4642c1e132efc953ac61ecb69
Moises Serrano <moyca777@gmail.com>
2020-08-15 00:45:07 -0300

    Update declare_payment_screen.dart

--- lib/DeclarePayment/ui/screens/declare_payment_screen.dart
+++ lib/DeclarePayment/ui/screens/declare_payment_screen.dart
@@ -1,20 +1,47 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:nextline/Tickets/ui/widgets/dropdown.dart';
 import 'package:nextline/utils/app_colors.dart';
+import 'package:nextline/utils/app_fonts.dart';
 
-class DeclarePayment extends StatefulWidget {
+class DeclarePaymentScreen extends StatefulWidget {
   @override
-  _DeclarePaymentState createState() => _DeclarePaymentState();
+  _DeclarePaymentScreenState createState() => _DeclarePaymentScreenState();
 }
 
-class _DeclarePaymentState extends State<DeclarePayment> {
+class _DeclarePaymentScreenState extends State<DeclarePaymentScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: AppColors.blue,
+      appBar: AppBar( backgroundColor: Color.fromRGBO(0, 109, 186, 1),),
+      body: Stack(
+        children: [
+          Container(
+            padding: EdgeInsets.all(10),
+            margin: EdgeInsets.fromLTRB(0, 20, 0, 0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  margin: EdgeInsets.fromLTRB(0, 10, 0, 15) ,
+                  child: Text(
+                    "Declarar Pago",
+                    style: TextStyle(color: AppColors.blue, fontSize: 21, fontFamily: AppFonts.fontTitle),
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.only(bottom: 27),
+                ),
+                Container(
+//                  margin: EdgeInsets.only(to),
+                  child: Expanded(
+                    child: DropdownWidget(hintText: "hola")
+                  ),
+                )
+              ],
+            ),
+          )
+        ],
       ),
-      body: ,
     );
   }
 }
