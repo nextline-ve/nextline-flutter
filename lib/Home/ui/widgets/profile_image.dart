4a59a154cb1a24c4e6219c0b6eb7f3f1ae0569e9
Oscar <oacs1221@gmail.com>
2020-08-16 15:58:56 -0400

    aceptar url en profile image

--- lib/Home/ui/widgets/profile_image.dart
+++ lib/Home/ui/widgets/profile_image.dart
@@ -7,14 +7,27 @@ import 'package:nextline/utils/app_colors.dart';
 
 class ProfileImageSelector extends StatefulWidget {
   final picker = ImagePicker();
-  ProfileImageSelector({Key key}) : super(key: key);
+  final File imageFile;
+  final String imgUrl;
+  final bool withAction;
+  final double size;
+  final Color color;
+  ProfileImageSelector({
+    Key key,
+    this.imageFile,
+    this.imgUrl,
+    this.color = AppColors.blue_dark,
+    this.size = 136,
+    this.withAction = true,
+  }) : super(key: key);
   @override
-  _ProfileImageSelectorState createState() => _ProfileImageSelectorState();
+  _ProfileImageSelectorState createState() =>
+      _ProfileImageSelectorState(imageFile: imageFile);
 }
 
 class _ProfileImageSelectorState extends State<ProfileImageSelector> {
   File imageFile;
-
+  _ProfileImageSelectorState({this.imageFile}) : super();
   void getImage(ImageSource source) {
     widget.picker.getImage(source: source).then((pickedFile) => setState(() {
           imageFile = File(pickedFile.path);
@@ -24,48 +37,56 @@ class _ProfileImageSelectorState extends State<ProfileImageSelector> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Expanded(
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                FlatButton(
-                  autofocus: false,
-                  clipBehavior: Clip.antiAliasWithSaveLayer,
-                  onPressed: () => {_showMyDialog()},
-                  child: imageFile != null
-                      ? ClipOval(
-                          child: Image.file(
-                            imageFile,
-                            height: 136,
-                            width: 136,
-                            fit: BoxFit.cover,
-                          ),
-                        )
-                      : Icon(
-                          Icons.account_circle,
-                          size: 136,
-                          color: AppColors.blue_dark,
-                        ),
-                ),
-                Container(
-                  padding: EdgeInsets.all(6),
-                  transform: Matrix4.translationValues(40, -40, 0),
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(24),
-                      color: AppColors.blue),
-                  child: Icon(
-                    Icons.camera_alt,
-                    size: 16,
-                    color: AppColors.white_color,
-                  ),
-                ),
-              ],
-            ),
-          ],
-        ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              FlatButton(
+                autofocus: false,
+                clipBehavior: Clip.antiAliasWithSaveLayer,
+                onPressed: () => {
+                  if (widget.withAction) {_showMyDialog()}
+                },
+                child: imageFile != null || widget.imgUrl != null
+                    ? ClipOval(
+                        child: imageFile != null
+                            ? Image.file(
+                                imageFile,
+                                height: widget.size,
+                                width: widget.size,
+                                fit: BoxFit.cover,
+                              )
+                            : Image.network(
+                                widget.imgUrl,
+                                height: widget.size,
+                                width: widget.size,
+                                fit: BoxFit.cover,
+                              ))
+                    : Icon(
+                        Icons.account_circle,
+                        size: widget.size,
+                        color: widget.color,
+                      ),
+              ),
+              widget.withAction
+                  ? Container(
+                      padding: EdgeInsets.all(6),
+                      transform: Matrix4.translationValues(40, -40, 0),
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(24),
+                          color: AppColors.blue),
+                      child: Icon(
+                        Icons.camera_alt,
+                        size: 16,
+                        color: AppColors.white_color,
+                      ),
+                    )
+                  : Text(""),
+            ],
+          ),
+        ],
       ),
     );
   }
