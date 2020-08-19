24ad74baf5f281d86b371af448a0a5dc361205f3
Moises Serrano <moyca777@gmail.com>
2020-08-18 23:51:12 -0300

    Update profile_screen.dart

--- lib/Profile/ui/screens/profile_screen.dart
+++ lib/Profile/ui/screens/profile_screen.dart
@@ -26,7 +26,7 @@ class _ProfileScreenState extends State<ProfileScreen> {
       body: Stack(
         alignment: Alignment.center,
         children: <Widget>[
-//          BackgroundTickets(),
+          BackgroundTickets(),
           Container(
             child: ListView(
               children: [
