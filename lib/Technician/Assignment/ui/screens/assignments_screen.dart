47ce32e1ed92e16695b958adfc58332d201eeeac
Oscar <oacs1221@gmail.com>
2020-09-13 13:52:58 -0400

    Logout Button & Behaviour

--- lib/Technician/Assignment/ui/screens/assignments_screen.dart
+++ lib/Technician/Assignment/ui/screens/assignments_screen.dart
@@ -6,6 +6,8 @@ import 'package:nextline/Tickets/model/model_ticket.dart';
 import 'package:nextline/Tickets/ui/widgets/ticket_row.dart';
 import 'package:nextline/utils/app_colors.dart';
 import 'package:nextline/utils/app_fonts.dart';
+import 'package:nextline/utils/app_session.dart';
+import 'package:nextline/widgets/confirmation_modal.dart';
 import 'package:nextline/widgets/jloading_screen.dart';
 import 'package:nextline/widgets/navigator_bar.dart';
 
@@ -23,14 +25,37 @@ class _AssignmentsScreen extends State<AssignmentsScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-          backgroundColor: AppColors.blue_dark,
-          centerTitle: true,
-          title: Text(
-            'ASIGNACIONES TÉCNICAS',
-            textAlign: TextAlign.center,
-            style: TextStyle(fontFamily: AppFonts.input, fontSize: 16),
-          ),
-          automaticallyImplyLeading: false),
+        backgroundColor: AppColors.blue_dark,
+        centerTitle: true,
+        title: Text(
+          'ASIGNACIONES TÉCNICAS',
+          textAlign: TextAlign.center,
+          style: TextStyle(fontFamily: AppFonts.input, fontSize: 16),
+        ),
+        automaticallyImplyLeading: false,
+        actions: [
+          GestureDetector(
+              onTap: () {
+                showConfirmationDialog(
+                    context,
+                    () => AppSession().unregister().then(
+                        (value) => Navigator.pushNamed(context, '/login')),
+                    () => Navigator.pop(context),
+                    title: Text("Cerrar Sesión"),
+                    content: Text("¿Está seguro de que desea salir?"));
+              },
+              child: Container(
+                  padding: EdgeInsets.all(10),
+                  child: Icon(
+                    Icons.exit_to_app,
+                    color: Colors.white,
+                  ),
+                  decoration: BoxDecoration(
+                    color: AppColors.blue_dark,
+                    borderRadius: BorderRadius.circular(10),
+                  ))),
+        ],
+      ),
       body: Stack(
         children: [
           Expanded(
