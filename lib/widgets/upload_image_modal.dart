f47c8ba19a4927970e6897d5726e0f35fa9f378e
abejarano <programador.angel@gmail.com>
2020-11-01 12:52:12 -0300

    cerrar el CupertinoActionSheetAction una ves seleccionada la opcion.

--- lib/widgets/upload_image_modal.dart
+++ lib/widgets/upload_image_modal.dart
@@ -11,73 +11,88 @@ Future<void> showMyDialog(context, text, getImage) async {
   final title = Text(text);
 
   if (Platform.isAndroid) {
-    return showDialog<void>(
-      context: context,
-      barrierDismissible: true, // user must tap button!
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: title,
-          content: SingleChildScrollView(
-            child: ListBody(
-              children: <Widget>[
-                content,
-              ],
-            ),
-          ),
-          actions: <Widget>[
-            RaisedButton(
-              color: AppColors.blue,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text('Camara'),
-                  Padding(
-                    padding: const EdgeInsets.only(top: 2, left: 5),
-                    child: Icon(Icons.camera_alt,
-                        size: 14, color: AppColors.white_color),
-                  )
-                ],
-              ),
-              onPressed: () {
-                getImage(ImageSource.camera);
-                Navigator.of(context).pop();
-              },
-            ),
-            RaisedButton(
-              color: AppColors.blue,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text('Galeria'),
-                  Padding(
-                    padding: const EdgeInsets.only(top: 2, left: 5),
-                    child: Icon(Icons.filter,
-                        size: 14, color: AppColors.white_color),
-                  )
-                ],
-              ),
-              onPressed: () {
-                getImage(ImageSource.gallery);
-                Navigator.of(context).pop();
-              },
-            ),
-          ],
-        );
-      },
-    );
+    showDialogAndroid(title, content, context, text, getImage);
+  } else {
+    showDialogIOS(title, content, context, text, getImage);
   }
 
+
+}
+showDialogAndroid(title, content, context, text, getImage) {
+  showDialog<void>(
+    context: context,
+    barrierDismissible: true, // user must tap button!
+    builder: (BuildContext context) {
+      return AlertDialog(
+        title: title,
+        content: SingleChildScrollView(
+          child: ListBody(
+            children: <Widget>[
+              content,
+            ],
+          ),
+        ),
+        actions: <Widget>[
+          RaisedButton(
+            color: AppColors.blue,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text('Cámara'),
+                Padding(
+                  padding: const EdgeInsets.only(top: 2, left: 5),
+                  child: Icon(Icons.camera_alt,
+                      size: 14, color: AppColors.white_color),
+                )
+              ],
+            ),
+            onPressed: () {
+              getImage(ImageSource.camera);
+              Navigator.of(context).pop();
+            },
+          ),
+          RaisedButton(
+            color: AppColors.blue,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text('Galería'),
+                Padding(
+                  padding: const EdgeInsets.only(top: 2, left: 5),
+                  child: Icon(Icons.filter,
+                      size: 14, color: AppColors.white_color),
+                )
+              ],
+            ),
+            onPressed: () {
+              getImage(ImageSource.gallery);
+              Navigator.of(context).pop();
+            },
+          ),
+        ],
+      );
+    },
+  );
+}
+
+showDialogIOS(title, content, context, text, getImage) {
   final act = CupertinoActionSheet(
       title: title,
       message: content,
       actions: <Widget>[
         CupertinoActionSheetAction(
-          child: Text("Camara"),
-          onPressed: () => {getImage(ImageSource.camera)},
+          child: Text("Cámara"),
+          onPressed: () {
+            getImage(ImageSource.camera);
+            Navigator.pop(context);
+          },
         ),
         CupertinoActionSheetAction(
-          child: Text("Galeria"),
-          onPressed: () => {getImage(ImageSource.gallery)},
+          child: Text("Galería"),
+          onPressed: () {
+            getImage(ImageSource.gallery);
+            Navigator.pop(context);
+          },
         )
       ],
       cancelButton: CupertinoActionSheetAction(
