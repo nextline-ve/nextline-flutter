e11eaab23204387d7bed0cb90b0432bd5f32a7a3
abejarano <programador.angel@gmail.com>
2021-01-04 14:41:15 -0300

    ajustes para produccion.

--- lib/utils/app_http.dart
+++ lib/utils/app_http.dart
@@ -7,7 +7,7 @@ import 'app_fonts.dart';
 
 abstract class AppHttp {
   Dio http = Dio();
-  String api = 'https://nextline.jaspesoft.com/api/v1/';
+  String api = 'https://panel.nextline.com.ve/api/v1/';
   Map<String, dynamic> header = {
     HttpHeaders.authorizationHeader:
         "Token ${(AppSession.data != null) ? AppSession.data.token : ''}"
